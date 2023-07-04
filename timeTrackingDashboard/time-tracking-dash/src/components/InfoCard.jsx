a7511adfcf48097fabb124dcef8dbaef7942bc8b
Nick Latcham <nick.latcham@proton.me>
2023-07-04 13:06:07 -0500

    .

--- timeTrackingDashboard/time-tracking-dash/src/components/InfoCard.jsx
+++ timeTrackingDashboard/time-tracking-dash/src/components/InfoCard.jsx
@@ -1,30 +1,9 @@
 import ellipsis from '../assets/images/icon-ellipsis.svg';
 
 const InfoCard = ({ data }) => {
-  let color;
 
-  switch (data.title) {
-    case 'Work':
-      color = 'work';
-      break;
-    case 'Play':
-      color = 'play';
-      break;
-    case 'Study':
-      color = 'study';
-      break;
-    case 'Exercise':
-      color = 'exercise';
-      break;
-    case 'Social':
-      color = 'social';
-      break;
-    case 'Self Care':
-      color = 'self-care';
-      break;
-    default:
-      color = 'white';
-  }
+  const color =
+    data.title !== 'Self Care' ? data.title.toLowerCase() : 'self-care';
 
   const cardAccent = `info info--${color}`;
 
@@ -40,10 +19,10 @@ const InfoCard = ({ data }) => {
               className="ellipsis"
             />
           </span>
-          <p className="info__fg--current">
+          <p className="info__fg-current">
             {data.timeframes.weekly.current}hrs
           </p>
-          <p className="info__fg--previous">
+          <p className="info__fg-previous">
             Last week - {data.timeframes.weekly.previous}
           </p>
         </div>
