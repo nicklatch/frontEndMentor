c761bec802523596588f8b646621724c7019c455
Nick <nick.latcham@proton.me>
2023-07-07 12:50:49 -0500

    fix(key): keys are now temp generated by data point

--- social-media-dash/src/components/TotalsDash/TotalsDash.jsx
+++ social-media-dash/src/components/TotalsDash/TotalsDash.jsx
@@ -4,7 +4,7 @@ const TotalsDash = ({ data }) => {
   return (
     <div className='totals__dash-grid'>
       {data.map((card) => (
-        <TotalsCard key={card} data={card} />
+        <TotalsCard key={card.totalFollowers} data={card} />
       ))}
     </div>
   );
