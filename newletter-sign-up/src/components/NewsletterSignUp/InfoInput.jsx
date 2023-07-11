a08ef85c8fb68b6f0f2b8adad0285afd7d9da720
Nick Latcham <nick.latcham@proton.me>
2023-07-10 20:12:58 -0500

    pushing to pull

--- newletter-sign-up/src/components/NewsletterSignUp/InfoInput.jsx
+++ newletter-sign-up/src/components/NewsletterSignUp/InfoInput.jsx
@@ -1,7 +1,7 @@
 const InfoInput = () => {
   return (
     <form className='newsletter__input'>
-      <label htmlFor='email__input'>email</label>
+      <label htmlFor='email__input'>Email adress</label>
       <input id='email__input' type='text' />
       <button type='submit'>Subscribe to monthly newsletter</button>
     </form>
